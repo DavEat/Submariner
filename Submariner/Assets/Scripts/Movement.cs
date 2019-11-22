5b8cebde0be5235bac9af12142c79d44a8f1512f
DavEat <david.mestdagh@hotmail.com>
2019-11-22 10:09:36 +0100

    Add basic movement for the player

---
+++ Submariner/Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float _movSpeed = 1;
+    [SerializeField] float _movSpeedAttenuationByRot = .5f;
+    [SerializeField] float _rotSpeed = 1;
+
+    [HideInInspector] public Vector3 destination;
+    Vector3 _lastDir;
+
+    Transform _transform;
+
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+        destination = _transform.position;
+    }
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 dir = destination - _transform.position;
+        if ((dir).sqrMagnitude > .2f)
+        {
+
+            Vector3 targetDir = destination - transform.position;
+
+            float step = _rotSpeed * Time.fixedDeltaTime;
+            //Find the rotation vector
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, .0f);
+            newDir.y = 0;
+
+            // Rotate the object by apply the rotation vector
+            transform.rotation = Quaternion.LookRotation(newDir);
+
+            float movSpeed = _movSpeed;
+            if (_lastDir != newDir) //check if the object is rotating
+            {
+                _lastDir = newDir;
+                movSpeed *= _movSpeedAttenuationByRot; //reduce speed if rotating
+            }
+
+            //move the object
+            transform.position += transform.forward * movSpeed * Time.fixedDeltaTime;
+        }
+    }
+}
---
+++ Submariner/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] LayerMask _layerMask = 0;
+
+    [SerializeField] Movement _applyOn = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, _layerMask))
+            {
+                _applyOn.destination = hit.point;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
